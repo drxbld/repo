79722da6bf4323b5e5be1e896438a9061a8e99fc
Derex <kodi.tech.help@gmail.com>
2018-09-24 21:02:05 -0700

    .

---
+++ script.module.resistance/lib/resources/lib/sources/pl/__init__.py
@@ -0,0 +1,10 @@
+
+ # resistance
+
+
+import os.path
+
+files = os.listdir(os.path.dirname(__file__))
+__all__ = [filename[:-3] for filename in files if not filename.startswith('__') and filename.endswith('.py')]
+
+
